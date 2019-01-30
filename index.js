b15cee1a3393c803e90b0b239f177ca65b080998
Michael Harrington <mike85h@gmail.com>
2019-01-30 16:33:30 -0700

    Added function to get balance in index.js

--- index.js
+++ index.js
@@ -1,15 +1,25 @@
 function issueNewToken(){
     fetch('https://ancient-headland-39950.herokuapp.com/api/newUser')
-  .then(res => {
-    if(res.ok) {
-      console.log(res);
-    } else {
-      throw Error(`Request rejected with status ${res.status}`);
-    }
-  })
-  .catch(console.error)
+        .then(res => {
+            if(res.ok) {
+                console.log(res);
+            } else {
+                throw Error(`Request rejected with status ${res.status}`);
+                alert('Something went wrong. Please try again.');
+            }
+        })
+        .catch(console.error)
 }
 
 function checkBalance(publicKey){
-
+    fetch(`https://ancient-headland-39950.herokuapp.com/api/getBalance/${publicKey}`)
+        .then(res => {
+            if(res) {
+                console.log(res);
+            } else {
+                throw Error(`Error: ${res}`)
+                alert('Something went wrong. Please try again.');
+            }
+        })
+        .catch(console.error)
 }
